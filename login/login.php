86ee7ae1609e4821be11a6f3d9809669ecd4885f
AzhrilLutfil15 <azrillutfipratama@gmail.com>
2026-09-11 13:34:18 +0700

    login

--- login/login.php
+++ login/login.php
@@ -1,6 +1,15 @@
 <?php
 session_start();
-include "../../config/koneksi.php";
+$host = "127.0.0.1";
+$user = "root";
+$password = "";
+$database = "booking_liburan";
+
+$koneksi = mysqli_connect($host, $user, $password, $database);
+
+if (!$koneksi) {
+    die("Koneksi database gagal: " . mysqli_connect_error());
+}
 
 /** @var mysqli $koneksi */ // <-- Tambahkan baris ini
 
